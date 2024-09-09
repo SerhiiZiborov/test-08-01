f8dc5ef4fca02cc651049f5b7f9dc087726de595
Ziborov Serhii <144032720+SerhiiZiborov@users.noreply.github.com>
2024-09-09 14:45:54 +0300

    update

--- js/task-01.js
+++ js/task-01.js
@@ -14,3 +14,24 @@ console.log(savedSettings);
 
 const parseSettings = JSON.parse(savedSettings);
 console.log(parseSettings);
+
+localStorage.clear();
+console.log(localStorage);
+
+// localStorage.setItem("ui-theme", "light");
+// localStorage.setItem("notif-level", "mute");
+
+// console.log(localStorage);
+// // Storage {notif-level: 'mute', ui-theme: 'light', length: 2}
+
+// localStorage.clear();
+// console.log(localStorage); // Storage {length: 0}
+
+// localStorage.setItem("ui-theme", "light");
+// localStorage.setItem("notif-level", "mute");
+
+// console.log(localStorage);
+// // Storage {notif-level: 'mute', ui-theme: 'light', length: 2}
+
+// localStorage.clear();
+// console.log(localStorage); // Storage {length: 0}
